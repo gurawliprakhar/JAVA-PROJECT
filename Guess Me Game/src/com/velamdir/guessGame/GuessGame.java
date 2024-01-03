98de881037910e773ef8bf055c1906aaea0a26ce
Prakhar Tripathi <110407222+gurawliprakhar@users.noreply.github.com>
2024-01-03 19:08:30 +0530

    Add files via upload

--- Guess Me Game/src/com/velamdir/guessGame/GuessGame.java
+++ Guess Me Game/src/com/velamdir/guessGame/GuessGame.java
@@ -1,31 +1,30 @@
 package com.velamdir.guessGame;
 
-import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
 public class GuessGame {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Guess the no Game");
 
-        System.out.println("May I have your name");
+    public static void playGuessingGame() {
+        System.out.println("Welcome to Guess the Number Game");
+
+        System.out.println("May I have your name?");
         Scanner scan = new Scanner(System.in);
         String name = scan.next();
         System.out.println("Hello " + name);
 
-        System.out.println("Shall we start");
+        System.out.println("Shall we start?");
         System.out.println("\t1.Yes");
         System.out.println("\t2.No");
 
         int beginAnswer = scan.nextInt();
 
-        while(beginAnswer != 1){
-            System.out.println("Shall we start");
+        while (beginAnswer != 1) {
+            System.out.println("Shall we start?");
             System.out.println("\t1.Yes");
             System.out.println("\t2.No");
 
             beginAnswer = scan.nextInt();
-
         }
 
         Random random = new Random();
@@ -37,29 +36,33 @@ public class GuessGame {
         boolean hasWon = false;
         boolean shouldFinish = false;
 
-        while(!shouldFinish){
-           timesTried++;
+        while (!shouldFinish) {
+            timesTried++;
 
-           if(timesTried < 5){
-              if(userInput == x){
-                 hasWon = true;
-                 shouldFinish = true;
-              } else if (userInput > x) {
-                  System.out.println("Guess Lower");
-                  userInput = scan.nextInt();
-              }else{
-                  System.out.println("Guess Higher");
-                  userInput = scan.nextInt();
-              }
-           }else{
-               shouldFinish = true;
-           }
+            if (timesTried < 5) {
+                if (userInput == x) {
+                    hasWon = true;
+                    shouldFinish = true;
+                } else if (userInput > x) {
+                    System.out.println("Guess Lower");
+                    userInput = scan.nextInt();
+                } else {
+                    System.out.println("Guess Higher");
+                    userInput = scan.nextInt();
+                }
+            } else {
+                shouldFinish = true;
+            }
         }
-        if(hasWon){
-            System.out.println("congratulation! you have guessed in your " + timesTried +"Guess. ");
-        }else{
-            System.out.println("Game Over! ");
+        if (hasWon) {
+            System.out.println("Congratulations! You have guessed the number in " + timesTried + " tries.");
+        } else {
+            System.out.println("Game Over!");
             System.out.println("The number was: " + x);
         }
+    }
+
+    public static void main(String[] args) {
+        playGuessingGame();
     }
 }
